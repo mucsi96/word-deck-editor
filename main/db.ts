6258ea0aeb879ee5274a7761d08e87338fde8915
Igor Mucsicska <mucsi_96@yahoo.com>
2020-05-03 12:00:21 +0200

    Fix db handling issue

--- main/db.ts
+++ main/db.ts
@@ -2,11 +2,13 @@ import { ipcMain } from "electron";
 import { resolve } from "path";
 import { Database } from "sqlite3";
 
-const db = new Database(resolve(__dirname, "../db.sqlite3"));
+const fileName = resolve(__dirname, "../db.sqlite3");
 
-ipcMain.handle("initDb", (event, args) => {
+ipcMain.handle("initDb", () => {
+  const db = new Database(fileName);
+
   db.serialize(() => {
-    db.run("CREATE TABLE lorem (info TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
   });
 
   db.close();
